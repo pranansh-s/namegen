5e761d21b509b5c68737b8ebd6d96fff9abdda3f
FeedoughAssets <feedoughassets@gmail.com>
2023-09-16 21:17:21 +0530

    wowo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const CompressionPlugin = require('compression-webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
@@ -11,6 +12,18 @@ module.exports = withBundleAnalyzer({
 		formats: ['image/avif', 'image/webp'],
 	},
 	env: {
-		NEXT_PUBLIC_ENV: 'PRODUCTION', // Your environment variables go here
+		NEXT_PUBLIC_ENV: 'PRODUCTION',
+	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+		  config.plugins.push(
+			new CompressionPlugin({
+			  test: /\.(js|css|svg|woff|woff2|eot|ttf|otf)$/,
+			  filename: '[path][base].gz',
+			})
+		  );
+		}
+	
+		return config;
 	},
 });
