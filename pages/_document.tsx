f24247e0518df57a9739cbab7634439edd8b77ee
FeedoughAssets <feedoughassets@gmail.com>
2023-09-29 21:55:26 +0530

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,21 @@ class MyDocument extends Document {
 					<link rel="icon" href="/favicon.ico" type="image/x-icon" />
 					<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 					<link rel="manifest" href="/manifest.json" />
+					<script
+						async
+						src="https://www.googletagmanager.com/gtag/js?id=G-QX9VX4NHZM"
+						/>
+						<script
+						dangerouslySetInnerHTML={{
+							__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+
+							gtag('config', 'G-QX9VX4NHZM');
+							`,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
